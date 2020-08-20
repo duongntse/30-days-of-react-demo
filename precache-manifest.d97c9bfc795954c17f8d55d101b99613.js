f0fda3a2fa18fad339a062ede9c43b2a56232eb4
Harry Nguyen <duongntse@gmail.com>
2020-08-20 21:12:25 +0700

    Rebuild website

---
+++ precache-manifest.d97c9bfc795954c17f8d55d101b99613.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31bb993bc45f21d21007f5508d98a198",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15a1808856c2338809a9",
+    "url": "/static/css/main.2b0f36ae.chunk.css"
+  },
+  {
+    "revision": "4e50ae5d304f48c3566e",
+    "url": "/static/js/2.ed6ff3e8.chunk.js"
+  },
+  {
+    "revision": "6137649d41939a8c4eeaf596da0d796e",
+    "url": "/static/js/2.ed6ff3e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15a1808856c2338809a9",
+    "url": "/static/js/main.8d35d802.chunk.js"
+  },
+  {
+    "revision": "c30de8ef62ba3f824d11",
+    "url": "/static/js/runtime-main.8fc9d66f.js"
+  }
+]);
